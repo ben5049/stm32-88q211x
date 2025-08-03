4e755ab2d2924c583dbf0ad8370b94ff615b7dc7
Ben <bens15351@gmail.com>
2025-08-03 02:26:05 +0100

    Added template .c and .h files

---
+++ Inc/88q211x.h
@@ -0,0 +1,23 @@
+/*
+ * 88q211x.h
+ *
+ *  Created on: Aug 3, 2025
+ *      Author: bens1
+ */
+
+#ifndef 88Q211X_INC_88Q211X_H_
+#define 88Q211X_INC_88Q211X_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* 88Q211X_INC_88Q211X_H_ */
---
+++ Src/88q211x.c
@@ -0,0 +1,10 @@
+/*
+ * 88q211x.c
+ *
+ *  Created on: Aug 3, 2025
+ *      Author: bens1
+ */
+
+
+
+
